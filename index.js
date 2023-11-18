593a923b5e68386507c2b115ca9f80aa13483769
paddyokore <paddyokore39@gmail.com>
2023-11-18 13:52:38 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+console.log('\n');
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+// Assignment
+// create a new, empty array to hold the messages
+const blank = [];
+
+function writeCards(peeps, event) {
+    for (let i = 0; i < peeps.length; i++) {
+        const msg = `Thank you, ${peeps[i]}, for the wonderful ${event} gift!`;
+        blank[i] = msg;
+    }
+    return blank;
+}
+
+// Assignment
+function countDown(n) {
+    let i = n;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }    
+}
+
